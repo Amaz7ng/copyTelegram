fa19f7d0bb4cefc1a2151f1c6cf4d241ef04f9f0
Amaz7ng <qute718@gmail.com>
2026-01-14 21:12:39 +0500

    try decide error with auth CI/CD

--- auth_service/users/producer.py
+++ auth_service/users/producer.py
@@ -6,26 +6,44 @@ from confluent_kafka import Producer
 
 logger = logging.getLogger('users')
 
+_producer = None
+
 def delivery_report(err, msg):
     if err is not None:
         logger.error(f"[Kafka] Ошибка доставки: {err}")
     else:
         logger.info(f"[Kafka] Доставлено в топик {msg.topic()}")
 
+def get_kafka_producer():
+    """Ленивая инициализация продюсера: создается только при реальном вызове"""
+    global _producer
+    
+    if 'pytest' in sys.modules or 'test' in sys.argv:
+        return None
+
+    if _producer is None:
+        try:
+            conf = {
+                'bootstrap.servers': 'kafka:9092',
+                'client.id': socket.gethostname(),
+                'api.version.request': True,
+                'broker.address.family': 'v4',
+                'socket.timeout.ms': 2000,
+            }
+            _producer = Producer(conf)
+        except Exception as e:
+            logger.error(f"[Kafka] Не удалось инициализировать продюсер: {e}")
+            return None
+    return _producer
+
 def publish_user_created_sync(user_data):
+    """Синхронная отправка сообщения в Kafka"""
     
-    if 'test' in sys.argv: # Если запущен pytest или manage.py test
+    p = get_kafka_producer()
+    
+    if p is None:
         return
-    """Синхронная отправка, идеальна для Celery"""
-    conf = {
-        'bootstrap.servers': 'kafka:9092',
-        'client.id': socket.gethostname(),
-        'api.version.request': True,
-        'broker.address.family': 'v4',
-        'socket.timeout.ms': 5000,
-    }
-    
-    p = Producer(conf)
+
     try:
         p.produce(
             'user_created',
@@ -33,6 +51,6 @@ def publish_user_created_sync(user_data):
             value=json.dumps(user_data).encode('utf-8'),
             callback=delivery_report
         )
-        p.flush(10)
+        p.flush(1)
     except Exception as e:
-        logger.error(f"[Kafka] Ошибка в продюсере: {e}")
+        logger.error(f"[Kafka] Ошибка при отправке сообщения: {e}")
